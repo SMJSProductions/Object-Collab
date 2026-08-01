04ffffb3d5cfbe8142fdcf846b475a3f91b697d1
SMJS <sjoerd.t@live.nl>
2026-08-01 17:49:26 +0200

    Made the vanilla warning android compatible

--- src/hooks/features/VanillaWarning.cpp
+++ src/hooks/features/VanillaWarning.cpp
@@ -5,8 +5,13 @@ using namespace object_collab;
 // Text object with the string: "This level contains\nmodded objects" on { x: 200, y: 45 } which is high detail
 static const std::string VANILLA_PLACEHOLDER = "1,914,2,200,3,45,103,1,31,VGhpcyBsZXZlbCBjb250YWlucwptb2RkZWQgb2JqZWN0cw==;";
 
+bool endsWithPlaceholder(const gd::string& str) {
+    // This is due to android support
+    return str.size() >= VANILLA_PLACEHOLDER.size() && str.compare(str.size() - VANILLA_PLACEHOLDER.size(), VANILLA_PLACEHOLDER.size(), VANILLA_PLACEHOLDER) == 0;
+}
+
 void cleanLevel(gd::string& levelString) {
-    if (levelString.ends_with(VANILLA_PLACEHOLDER)) {
+    if (endsWithPlaceholder(levelString)) {
         levelString.erase(levelString.size() - VANILLA_PLACEHOLDER.size());
     }
 }
@@ -25,14 +30,16 @@ void VanillaModLevelEditorLayer::createObjectsFromSetup(gd::string& levelString)
 
 gd::string VanillaModLevelEditorLayer::getLevelString() {
     if (CustomLevelData::raw(this).size()) {
-        gd::string levelString = LevelEditorLayer::getLevelString();
+        std::string levelString = LevelEditorLayer::getLevelString();
 
-        if (levelString.ends_with(VANILLA_PLACEHOLDER)) {
+        if (endsWithPlaceholder(levelString)) {
             return levelString;
         } else {
-            levelString.append(VANILLA_PLACEHOLDER);
-
-            return levelString;
+            #ifdef GEODE_IS_ANDROID
+                return std::move(levelString) + VANILLA_PLACEHOLDER;
+            #else
+                return levelString.append(VANILLA_PLACEHOLDER);
+            #endif
         }
     } else {
         return LevelEditorLayer::getLevelString();
