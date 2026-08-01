1a887ace0491086f377c22ef918d966f9288cf80
SMJS <sjoerd.t@live.nl>
2026-08-01 18:32:05 +0200

    Bad code to try and solve android incompats on the vanilla warning

--- src/hooks/features/VanillaWarning.cpp
+++ src/hooks/features/VanillaWarning.cpp
@@ -5,34 +5,32 @@ using namespace object_collab;
 // Text object with the string: "This level contains\nmodded objects" on { x: 200, y: 45 } which is high detail
 static const std::string VANILLA_PLACEHOLDER = "1,914,2,200,3,45,103,1,31,VGhpcyBsZXZlbCBjb250YWlucwptb2RkZWQgb2JqZWN0cw==;";
 
-bool endsWithPlaceholder(const gd::string& str) {
-    #ifdef GEODE_IS_ANDROID
-        return std::string_view(str).ends_with(VANILLA_PLACEHOLDER);
-    #else
-        return str.ends_with(VANILLA_PLACEHOLDER);
-    #endif
+// This mostly exists for Android support
+bool endsWithPlaceholder(const std::string_view levelString) {
+    return levelString.ends_with(VANILLA_PLACEHOLDER);
 }
 
-void cleanLevel(gd::string& levelString) {
+gd::string& cleanLevel(gd::string& levelString) {
+    #define levelStringName GEODE_CONCAT(levelString, GEODE_ANDROID(Android))
+
     if (endsWithPlaceholder(levelString)) {
         #ifdef GEODE_IS_ANDROID
-            levelString = levelString.substr(0, levelString.size() - VANILLA_PLACEHOLDER.size());
-        #else
-            levelString.erase(levelString.size() - VANILLA_PLACEHOLDER.size());
+            std::string levelStringName = levelString;
         #endif
+
+        levelStringName.erase(levelString.size() - VANILLA_PLACEHOLDER.size());
+        GEODE_ANDROID(levelString = levelStringName);
     }
+
+    return levelString;
 }
 
 void VanillaModPlayLayer::prepareCreateObjectsFromSetup(gd::string& levelString) {
-    cleanLevel(levelString);
-
-    return PlayLayer::prepareCreateObjectsFromSetup(levelString);
+    return PlayLayer::prepareCreateObjectsFromSetup(cleanLevel(levelString));
 }
 
 void VanillaModLevelEditorLayer::createObjectsFromSetup(gd::string& levelString) {
-    cleanLevel(levelString);
-
-    return LevelEditorLayer::createObjectsFromSetup(levelString);
+    return LevelEditorLayer::createObjectsFromSetup(cleanLevel(levelString));
 }
 
 gd::string VanillaModLevelEditorLayer::getLevelString() {
